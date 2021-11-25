ecfe8e014d83274e74e1575e40eeffd1dc73b34a
Sahar <saharelbaz18@gmail.com>
2021-11-25 18:54:09 +0200

    Changes by abusa2

--- Player.h
+++ Player.h
@@ -8,12 +8,17 @@
 #include "GroupPlayer.h"
 class Player{
 
-    int PlayerID;
-    int GroupID;
-    int Level;
+    int playerID;
+    int groupID;
+    int level;
     AvlTree<GroupPlayer>* location;
 
-
+    int getPlayerID() const;
+    int getGroupID() const;
+    int getLevel() const;
+    void setPlayerID(int playerID) const;
+    void setGroupID(int groupID) const;
+    void setLevel(int level) const;
 
 };
 
